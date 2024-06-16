10f04dac4ca68f4270124de4c0ec7d3b37e3a3b1
Tadeusz Nowak <81537802+php-vyvo@users.noreply.github.com>
2024-06-16 12:23:13 +0200

    feat(render): added render hangman

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 #include <cstdlib>
 #include <ctime>
+#include <iostream>
 
 void Game::OnInit()
 {
@@ -8,6 +9,16 @@ void Game::OnInit()
 	srand(std::time(NULL));
 	int randomIndex = rand() % m_wordsPool.size();
 	m_word = m_wordsPool[randomIndex];
+
+	m_hangmanBox.reserve(Game::MAX_ELEMENTS);
+
+	for (int row = 0; row < Game::ROWS; ++row) {
+		for (int column = 0; column < Game::COLUMNS; ++column) {
+			m_hangmanBox.push_back(' ');
+		}
+	}
+
+	m_failAnswerCount = 0;
 }
 
 void Game::OnInput()
@@ -17,20 +28,29 @@ void Game::OnInput()
 
 bool Game::OnUpdate(float deltaTime)
 {
-
-
-
+	if (m_answerFail) {
+		for (int i = 0; i < m_hangman[m_failAnswerCount].size(); ++i) {
+			int index = m_hangman[m_failAnswerCount][i].position.row * Game::COLUMNS + m_hangman[m_failAnswerCount][i].position.column;
+			m_hangmanBox[index] = m_hangman[m_failAnswerCount][i].c;
+		}
+		++m_failAnswerCount;
+		m_answerFail = false;
+	}
 
 	return false;
 }
 
 void Game::OnRender()
 {
-
+	for (int i = 0; i < Game::ROWS; ++i) {
+		for (int j = 0; j < Game::COLUMNS; ++j) {
+			std::cout << m_hangmanBox[i * Game::COLUMNS + j];
+		}
+		std::cout << std::endl;
+	}
 }
 
 void Game::OnShutdown()
 {
 	m_guessedLetters.clear();
-
 }
--- game.h
+++ game.h
@@ -2,6 +2,17 @@
 #include <string>
 #include <vector>
 
+struct Position
+{
+	int row;
+	int column;
+};
+
+struct HangmanPart
+{
+	Position position;
+	char c;
+};
 
 class Game
 {
@@ -13,6 +24,10 @@ class Game
 	};
 
 public:
+	static const int COLUMNS = 9;
+	static const int ROWS = 8;
+	static const int MAX_ELEMENTS = COLUMNS * ROWS;
+
 	void OnInit();
 	void OnInput();
 	bool OnUpdate(float deltaTime);
@@ -24,5 +39,56 @@ private:
 	std::vector<std::string> m_wordsPool = { "kolor", "silnik", "wzorzec", "silnie", "kanapka" };
 	std::vector<bool> m_guessedLetters;
 	GameState m_gameState = GameState::START;
-
+	int m_failAnswerCount;
+	int m_answerFail = false;
+	std::vector<std::vector<HangmanPart>> m_hangman{ 
+		{
+			HangmanPart{Position{7,0}, '/'}
+		},
+		{
+			HangmanPart{Position{7,2}, '\\'}
+		},
+		{
+			HangmanPart{Position{6,1}, '|'},
+			HangmanPart{Position{5,1}, '|'},
+		},
+		{
+			HangmanPart{Position{4,1}, '|'},
+			HangmanPart{Position{3,1}, '|'},
+		},
+		{
+			HangmanPart{Position{2,1}, '|'},
+			HangmanPart{Position{1,1}, '|'},
+		},
+		{
+			HangmanPart{Position{0,2}, '_'},
+			HangmanPart{Position{0,3}, '_'},
+			HangmanPart{Position{0,4}, '_'},
+			HangmanPart{Position{0,5}, '_'},
+			HangmanPart{Position{0,6}, '_'},
+		},
+		{
+			HangmanPart{Position{1,6}, '|'},
+		},
+		{
+			HangmanPart{Position{2,6}, 'O'},
+		},
+		{
+			HangmanPart{Position{3,6}, '|'},
+			HangmanPart{Position{4,6}, '|'},
+		},
+		{
+			HangmanPart{Position{3,5}, '\\'},
+		},
+		{
+			HangmanPart{Position{3,7}, '/'},
+		},
+		{
+			HangmanPart{Position{5,5}, '/'},
+		},
+		{
+			HangmanPart{Position{5,7}, '\\'},
+		},
+	};
+	std::vector<char> m_hangmanBox;
 };
